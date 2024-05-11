0c9781af4b8fd6773f41c2e4212a4b1bc9de2317
Hayato Muraki <developer.hayato.m@gmail.com>
2024-05-12 00:40:02 +0900

    add post.js

---
+++ static/js/post.js
@@ -0,0 +1,53 @@
+// 画像をリサイズして、HTMLで表示する
+$(function () {
+    var file = null;
+    var blob = null;
+
+    $("input[type=file]").change(function () {
+        file = $(this).prop("files")[0];
+
+        // ファイルチェック
+        if (!file.name.endsWith(".nii")) {
+            file = null;
+            blob = null;
+            alert("niiファイルを選択してください");
+            return; // ファイルが適切でない場合、ここで処理を終了
+        }
+
+        // ファイルが適切な場合、Blobを作成
+        blob = new Blob([file], { type: "application/octet-stream" });
+    });
+  
+    // アップロード開始ボタンがクリックされたら
+    $("#post").click(function () {
+        if (!file || !blob) {
+            return; // ファイルまたはBlobがなければ何もしない
+        }
+
+        var fd = new FormData();
+        fd.append("files", blob, file.name);
+
+        // API宛にPOSTする
+        $.ajax({
+            url: "/api/image_recognition",
+            type: "POST",
+            dataType: "json",
+            data: fd,
+            processData: false,
+            contentType: false,
+        })
+        .done(function (data, textStatus, jqXHR) {
+            // 通信が成功した場合、結果を出力する
+            console.log(data);
+            var result = document.getElementById("result");
+            result.innerHTML = "この患者の画像...「" + data.response + "」と類似性あり";
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            // 通信が失敗した場合、エラーメッセージを出力する
+            var result = document.getElementById("result");
+            result.innerHTML = "サーバーとの通信が失敗した...";
+        });
+    });
+});
+  
+  
